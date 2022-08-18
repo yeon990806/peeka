91c2b42e811f6832d8047b74dca44608a459d91b
yeon990806 <yeon990806@gmail.com>
2022-08-18 17:30:19 +0900

    fix - Applayout fetch code comment

--- src/layout/AppLayout/index.tsx
+++ src/layout/AppLayout/index.tsx
@@ -1,16 +1,17 @@
 
-import style from "./style.module.scss"
 import { useState, useEffect, useCallback, createRef } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { FETCH_ALERT_REQUEST } from "@/store/reducer/user";
+import { TOGGLE_SIGN_IN_POPUP } from "@/store/reducer/popup";
+import { useMediaQuery } from "react-responsive";
+import { StateType } from "@/common/defines/Store";
 import TopButton from "@/components/TopButton";
 import PrimaryHeader from "@/components/PrimaryHeader";
-import { useMediaQuery } from "react-responsive";
-import { useDispatch, useSelector } from "react-redux";
 import VideoList from "@/components/VideoList";
-import { FETCH_ALERT_REQUEST } from "@/store/reducer/user";
-import { StateType } from "@/common/defines/Store";
 import CuratorList from "@/components/CuratorList";
 import SignInPopup from "@/components/SignInPopup";
-import { TOGGLE_SIGN_IN_POPUP } from "@/store/reducer/popup";
+
+import style from "./style.module.scss"
 
 interface AppLayoutProps {
   children: React.ReactNode,
@@ -27,12 +28,12 @@ const AppLayout = (props: AppLayoutProps) => {
     query: '(min-width: 1400px)',
   })
 
-  const fetchAlertList = () => dispatch({
-    type: FETCH_ALERT_REQUEST,
-    data: {
-      id: (userInfo && (userInfo.alertList && userInfo.alertList.length > 0)) ? userInfo.alertList[userInfo.alertList.length - 1].id : ''
-    }
-  })
+  // const fetchAlertList = () => dispatch({
+  //   type: FETCH_ALERT_REQUEST,
+  //   data: {
+  //     id: (userInfo && (userInfo.alertList && userInfo.alertList.length > 0)) ? userInfo.alertList[userInfo.alertList.length - 1].id : ''
+  //   }
+  // })
 
   const toggleSignInPopup = () => dispatch({
     type: TOGGLE_SIGN_IN_POPUP,
@@ -55,7 +56,7 @@ const AppLayout = (props: AppLayoutProps) => {
     // let watch = null
 
     setIsShowing(true)
-    if (userInfo.id) fetchAlertList()
+    // if (userInfo.id) fetchAlertList()
     // if (container.current) {
     //   const watch = () => {
     //     container.current.addEventListener('scroll', onFollow)
@@ -81,8 +82,8 @@ const AppLayout = (props: AppLayoutProps) => {
           { props.children }
         </div>
       </div>
-      { wideScreen && <VideoList /> }
-      { wideScreen && <CuratorList /> }
+      {/* { wideScreen && <VideoList /> }
+      { wideScreen && <CuratorList /> } */}
       <SignInPopup
         display={ signInPopupDisplay }
         onClose={ toggleSignInPopup }
