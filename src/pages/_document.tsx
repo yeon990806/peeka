1d8665a550616cfac1250c2342819d9447725456
yeon990806 <yeon990806@gmail.com>
2022-08-22 22:04:16 +0900

    description 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,14 @@ class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" />
           <title>Peeka</title>
+          <meta name="keywords" content="영화 드라마 애니메이션 만화 웹툰 웹소설 피카 SNS 소셜미디어 커뮤니티 추천"></meta>
+          <meta name="description" content="영화, 드라마, 웹툰, 웹소설을 소재로 소통하는 공간"></meta>
+          <meta name="author" content="peeka corp."></meta>
+          <meta property="og:type" content="website"></meta>
+          <meta property="og:title" content="피카"></meta>
+          <meta property="og:description" content="영화, 드라마, 웹툰, 웹소설을 소재로 소통하는 공간"></meta>
+          <meta property="og:image" content="/images/peeka-logo.svg"></meta>
+          <meta property="og:url" content="http://www.peeka.ai"></meta>
         </Head>
         <body>
           <Main />
