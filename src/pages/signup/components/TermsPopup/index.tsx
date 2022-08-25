832d96b3e01ad47932e990a31dd9dac4df4c89b6
yeon990806 <yeon990806@gmail.com>
2022-08-26 00:01:59 +0900

    Fix - 약관 제거

--- src/pages/signup/components/TermsPopup/index.tsx
+++ src/pages/signup/components/TermsPopup/index.tsx
@@ -39,7 +39,6 @@ const TermsPopup = (props: TermsPopupProps) => {
         return (
           <div className={ style.Terms }>
             <iframe src="https://docs.google.com/document/d/e/2PACX-1vQcqgmUf6LEM2Jn6yrteh5LgeHJXLj4qViP9vwKTZFKnkb0hCsaYjprpEpb2ypzRjMHokvPHnbOpdDB/pub?embedded=true"></iframe>
-            <iframe src="https://docs.google.com/document/d/e/2PACX-1vS_bvPxpF35OPQwAJwg26rcvwsem_D32k0zMpUrikflimuER2jDOswaLTYAEZ5JaG1jgJQdVYvLv3SN/pub?embedded=true"></iframe>
           </div>
         )
     }
