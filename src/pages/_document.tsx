b8967d22c2f73987c8274343c1baaa5ac5643ad8
yeon990806 <yeon990806@gmail.com>
2022-10-29 21:52:34 +0900

    Feat - 검색 관련 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" />
           <title>Peeka</title>
+          <meta name="robots" content="index,follow" />
           <meta name="keywords" content="영화 드라마 애니메이션 만화 웹툰 웹소설 피카 SNS 소셜미디어 커뮤니티 추천"></meta>
           <meta name="description" content="영화, 드라마, 웹툰, 웹소설을 소재로 소통하는 공간"></meta>
           <meta name="author" content="peeka corp."></meta>
